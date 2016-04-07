caabf93ae62901887e3a763c99fd4b7e284e365c
ketterin <Richard.Kettering@biworldwide.com>
2016-04-07 17:30:18 -0500

    Working fizzbuzz implementation.

--- elm fizzbuzz/Main.elm
+++ elm fizzbuzz/Main.elm
@@ -4,17 +4,24 @@ import Color exposing (..)
 --import Graphics.Collage exposing (..)
 import Html exposing (text)
 import Array exposing (..)
+import List exposing (..)
 import String exposing (..)
 
-{-
-diamond color size =
-  square size |> filled color |> rotate (degrees 45)
+
+text_for_num : Int -> String
+text_for_num inputVal = 
+  if (((inputVal % 5) == 0) && ((inputVal % 3) == 0)) then
+    (toString inputVal) ++ " FizzBuzz"
+  else if ((inputVal % 5) == 0) then
+    (toString inputVal) ++ " Fizz"
+  else if ((inputVal % 3) == 0) then
+    (toString inputVal) ++ " Buzz"
+  else
+    (toString inputVal) ++ " ____"
 
 
-spawn_text text = 
-  map ("hello there") (initialize 4 identity)
--}
 
 main = 
-  text (String.join ", " (Array.toList (Array.map toString (initialize 4 identity))))
+ text (String.join ", " (List.map text_for_num [0..20]))
+--  text (String.join ", " (Array.toList (Array.map toString (initialize 4 identity))))
 --    text (String.fromList (Array.toList (Array.map toString (initialize 4 identity))))
